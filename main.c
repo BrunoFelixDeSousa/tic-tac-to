9802afcb44de93bbeef0ccef290cd48de59b9492
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2022-01-12 23:06:35 -0300

    criado função resetBoard

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <ctype.h>
 #include <time.h>
 
+// variáveis global
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
 void resetBoard();
 void printBoard();
 int checkFreeSpaces();
@@ -14,6 +19,11 @@ void printWinner(char);
 
 int main()
 {
+    char winner = ' ';
+
+
+    resetBoard();
+    printBoard();
     
 
     return 0;
@@ -21,7 +31,12 @@ int main()
 
 void resetBoard()
 {
-
+    // loop para deixar o tabuleiro limpo
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
 }
 
 void printBoard()
