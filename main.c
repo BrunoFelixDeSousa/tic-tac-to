28a390a3eaed803f0bbd520c9a3ec3ea8a29b735
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2022-01-12 23:27:18 -0300

    criado função checkFreeSpaces

--- main.c
+++ main.c
@@ -23,7 +23,11 @@ int main()
 
 
     resetBoard();
-    printBoard();
+
+    while(winner == ' ' && checkFreeSpaces() != 0) {
+        printBoard();
+    }
+    
     
 
     return 0;
@@ -51,7 +55,17 @@ void printBoard()
 
 int checkFreeSpaces()
 {
+    int freeSpaces = 9;
 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] != ' ') {
+                freeSpaces--;
+            }
+        }
+    }
+
+    return freeSpaces;
 }
 
 void playerMove()
